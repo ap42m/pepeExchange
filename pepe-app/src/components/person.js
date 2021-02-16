8070c884335f5128acbb7c6e876273b9027dbecd
Adam Manley <adam.p.manley@gmail.com>
2021-02-16 14:19:13 +0000

    added person component
    added a component that displays a persons name and retrieves the pet function

---
+++ pepe-app/src/components/person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Pet from './pet';
+
+const Person = (props) => {
+    return(
+        <div>
+            <p>My Name is {props.name}</p>
+            <Pet petName = {props.pet} />
+        </div>
+    )
+}
